e2e9a2b5f6b963d1a914b24d5d6c29fac41c79e3
isaach3 <isaach3@uci.edu>
2025-03-07 13:19:40 -0800

    cleaned up nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,18 +1,3 @@
-// const hamburger = document.querySelector('.hamburger');
-// const navLinks = document.querySelector('.nav-links');
-// let menuOpen = false;
-
-// hamburger.addEventListener('click', () => {
-//     if (menuOpen == false) {
-//         navLinks.style.display = "block";
-//         menuOpen = true;
-//     }
-//     else if (menuOpen == true) {
-//         navLinks.style.display = "none";
-//         menuOpen = false;
-//     }   
-// });
-
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
     const navLinks = document.querySelector('.nav-links');
