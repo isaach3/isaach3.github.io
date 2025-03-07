603cc0b6d1c07b262c65cb31505ca871b1842737
isaach3 <isaach3@uci.edu>
2025-03-06 17:46:28 -0800

    attempt to fix hamburger bug

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,14 +1,23 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-let menuOpen = false;
+// const hamburger = document.querySelector('.hamburger');
+// const navLinks = document.querySelector('.nav-links');
+// let menuOpen = false;
 
-hamburger.addEventListener('click', () => {
-    if (menuOpen == false) {
-        navLinks.style.display = "block";
-        menuOpen = true;
-    }
-    else if (menuOpen == true) {
-        navLinks.style.display = "none";
-        menuOpen = false;
-    }   
+// hamburger.addEventListener('click', () => {
+//     if (menuOpen == false) {
+//         navLinks.style.display = "block";
+//         menuOpen = true;
+//     }
+//     else if (menuOpen == true) {
+//         navLinks.style.display = "none";
+//         menuOpen = false;
+//     }   
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
 });
