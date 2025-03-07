54a8d76d63757ed5db9324cbeb9d60d6b1fe34da
isaach3 <isaach3@uci.edu>
2025-03-06 17:25:59 -0800

    fixed hamburger button

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector(' hamburger');
+const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 let menuOpen = false;
 
